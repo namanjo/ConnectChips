50b880be03d0ec1a1c3c72eb6598d8848037c8fc
Naman Joshi <naman14joshi@gmail.com>
2019-10-10 23:57:06 +0530

    Added all the Files & inserted some code in script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var player1 = prompt("Player 1: Enter your name, you will be Blue");
+var player1color = 'rgb(5, 130, 255)';
+
+var player2 = prompt('Player 2: Enter Your name, you will be red');
+var player2color = 'rgb(215, 23, 23)';
+
+var game_on = true;
+var table = $('table tr');
+console.log(table);
