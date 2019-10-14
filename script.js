70b5bb0bb759c36e283a1ccedcbd45e9c174cbb0
Naman Joshi <naman14joshi@gmail.com>
2019-10-14 21:54:24 +0530

    Created all the Functionalities and workflow.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-var player1 = prompt("Player 1: Enter your name, you will be Blue");
-var player1color = 'rgb(5, 130, 255)';
+request = 'empty';
 
-var player2 = prompt('Player 2: Enter Your name, you will be red');
+while (request !== 'quit') {
+  var player1 = prompt("Player 1: Enter your name, you will be Blue");
+  var player2 = prompt('Player 2: Enter Your name, you will be red');
+
+  if (player1 === null || player2 === null) {
+    continue
+  }
+  else {
+    request = 'quit';
+  }
+}
+
+var player1color = 'rgb(5, 130, 255)';
 var player2color = 'rgb(215, 23, 23)';
 
 var game_on = true;
@@ -19,7 +30,6 @@ function checkBottom(colIndex){
   var colorReport = returnColor(5, colIndex);
   for (var row = 5; row > -1; row--) {
     colorReport = returnColor(row, colIndex);
-
     if (colorReport === 'rgb(241, 241, 241)') {
       return row;
     }
@@ -31,7 +41,7 @@ function colorMatchCheck(one, two, three, four){
 }
 
 function horizontalWinCheck(){
-  for(var row = 5; row < -1; row--){
+  for(var row = 0; row < 6; row++){
     for(var col = 0; col < 4; col++){
       if(colorMatchCheck(returnColor(row, col), returnColor(row, col+1), returnColor(row, col+2), returnColor(row, col+3))){
         return true;
@@ -44,8 +54,8 @@ function horizontalWinCheck(){
 
 function verticalWinCheck(){
   for(var col = 0; col < 7; col++){
-    for(var row = 5; row < 2; row--){
-      if(colorMatchCheck(returnColor(row, col), returnColor(row-1, col), returnColor(row-2, col), returnColor(row, col-3))){
+    for(var row = 0; row < 3; row++){
+      if(colorMatchCheck(returnColor(row, col), returnColor(row+1, col), returnColor(row+2, col), returnColor(row+3, col))){
         return true;
       }else {
         continue;
@@ -53,3 +63,34 @@ function verticalWinCheck(){
     }
   }
 }
+
+var currentPlayer = 1;
+var currentName = player1;
+var currentColor = player1color;
+$('#ticker').text(player1 + " it is your turn, choose a column to drop chip.")
+
+$('.board button').on('click', function(){
+
+  var col = $(this).closest('td').index();
+  var bottomEmptyChip = checkBottom(col);
+
+  changeColor(bottomEmptyChip, col, currentColor);
+
+  if(horizontalWinCheck() || verticalWinCheck()){
+    $('h1').css({'font-size':'24px', 'font-family':'garmond'});
+    $('h1').text(currentName + " has won the game, Refresh the page to play again..!");
+    $('h2').fadeOut('9000');
+  }
+
+  currentPlayer = currentPlayer * -1;
+
+  if (currentPlayer === 1) {
+    currentName = player1;
+    $('#ticker').text(currentName + " it is your turn.");
+    currentColor = player1color;
+  }else {
+    currentName = player2;
+    $('#ticker').text(currentName+ " it your turn.")
+    currentColor = player2color;
+  }
+})
